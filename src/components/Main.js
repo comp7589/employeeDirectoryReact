8adb6a3f40f0ce9b0802d87a6c6ce3f29d150c51
Ryan Compton <ryan.wade.compton@gmail.com>
2020-04-29 16:24:26 -0600

    outline component structure. build base App.js.

---
+++ src/components/Main.js
@@ -0,0 +1,5 @@
+import React from "react";
+// import DataArea from "./components/DataArea.js";
+
+//main < dataarea < datatable, navbar(searchbar here), api(from util) < databody(export to datatable)  
+//header, wrapper, footer(optional), searchbar
