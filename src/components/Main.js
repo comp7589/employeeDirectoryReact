6347b925f4a37133cdaf69378f932639d23d9e72
Ryan Compton <ryan.wade.compton@gmail.com>
2020-04-29 22:06:19 -0600

    Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,14 @@
 import React from "react";
-// import DataArea from "./components/DataArea.js";
+import DataArea from "./components/DataArea.js";
 
 //main < dataarea < datatable, navbar(searchbar here), api(from util) < databody(export to datatable)  
 //header, wrapper, footer(optional), searchbar
+
+
+function Main() {
+    return(
+        <DataArea />
+    );
+};
+
+export default Main;
